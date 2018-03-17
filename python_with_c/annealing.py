dfe11534a34b558563a47034412f20bd8ebef67e
heathersky <36645518+heathersky@users.noreply.github.com>
2018-03-16 23:26:29 -0400

    Add files via upload
    magnetic annealing added. Works great.

--- python_with_c/annealing.py
+++ python_with_c/annealing.py
@@ -2,7 +2,7 @@ def T_anneal(T, step, num_steps, num_burnin):
 
     #implement annealing code here
     T_a = T
-    T_0 = 20
+    T_0 = T_a+1
     m = -T_0/num_burnin
     T_step = m*step+T_0
 
@@ -13,5 +13,8 @@ def B_anneal(B, step, num_steps, num_burnin):
     #implement annealing code here
     
     B_a = B
+    B_0 = B_a+1
+    s = -B_0/num_burnin
+    B_step = s*step+B_0
 
-    return float(B_a)
+    return float(max(B_step,B_a))
